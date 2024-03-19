dc341f0a40ce31524de5fec46aba5e113762a55c
DatNguynn <115922650+DatNguynn@users.noreply.github.com>
2024-03-19 22:06:41 +0700

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,10 +2,12 @@ import axios from "axios";
 
 const storedToken = localStorage.getItem("token") || null;
 let access_token = "";
-if (storedToken !== "null"  && storedToken.length > 0) {
+console.log(storedToken);
+console.log(typeof storedToken);
+
+if (storedToken !== 'undefined' && storedToken !== "null"  && storedToken.length > 0) {
     access_token = JSON.parse(storedToken).access_token;
 }
-console.log(access_token);
 export const makeRequest = axios.create({
 
   baseURL: "http://localhost:5000",
