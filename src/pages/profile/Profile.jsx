e3c8545140b4401a45fb913cb32fe0ecda8c05ca
Nguyễn Ngọc Đức <duco060303@gmail.com>
2024-04-03 20:42:51 +0700

    update feature logout

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,11 +1,6 @@
 import "./profile.scss";
-import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import PinterestIcon from "@mui/icons-material/Pinterest";
-import TwitterIcon from "@mui/icons-material/Twitter";
 import PlaceIcon from "@mui/icons-material/Place";
-import LanguageIcon from "@mui/icons-material/Language";
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Posts from "../../components/posts/Posts";
@@ -19,7 +14,7 @@ import { useState } from "react";
 
 const Profile = () => {
   const [openUpdate, setOpenUpdate] = useState(false);
-  const { currentUser } = useContext(AuthContext);
+  const { currentUser, setTokenAndUser } = useContext(AuthContext);
   const axiosPrivate = useAxiosPrivate();
 
   const userId = parseInt(useLocation().pathname.split("/")[2]);
@@ -58,6 +53,10 @@ const Profile = () => {
   //   mutation.mutate(relationshipData.includes(currentUser.id));
   // };
 
+    const logoutHandel = () => {
+      setTokenAndUser(null, null)
+    }
+
   return (
     <div className="profile">
       {isLoading ? (
@@ -70,34 +69,20 @@ const Profile = () => {
           </div>
           <div className="profileContainer">
             <div className="uInfo">
-              <div className="left">
-                <a href="http://facebook.com">
-                  <FacebookTwoToneIcon fontSize="large" />
-                </a>
-                <a href="http://facebook.com">
-                  <InstagramIcon fontSize="large" />
-                </a>
-                <a href="http://facebook.com">
-                  <TwitterIcon fontSize="large" />
-                </a>
-                <a href="http://facebook.com">
-                  <LinkedInIcon fontSize="large" />
-                </a>
-                <a href="http://facebook.com">
-                  <PinterestIcon fontSize="large" />
-                </a>
-              </div>
               <div className="center">
-                <span>{data.name}</span>
+                <span>{data.username}</span>
                 <div className="info">
                   <div className="item">
                     <PlaceIcon />
                     <span>{data.city}</span>
                   </div>
-                  <div className="item">
-                    <LanguageIcon />
-                    <span>{data.website}</span>
-                  </div>
+                  {
+                    currentUser.id === data.id && (
+                      <button className="item button-exit" onClick={logoutHandel}>
+                        <ExitToAppIcon />
+                      </button>
+                    )
+                  }
                 </div>
                 {/* {rIsLoading ? (
                   "loading"
@@ -111,10 +96,10 @@ const Profile = () => {
                   </button>
                 )*/}
               </div>
-              <div className="right">
+              {/* <div className="right">
                 <EmailOutlinedIcon />
                 <MoreVertIcon />
-              </div>
+              </div> */}
             </div>
             <Posts userId={userId} />
           </div>
