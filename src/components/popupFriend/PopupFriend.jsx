c3f07641b3dfb7f3010f85427c15b6ee88967321
Nguyễn Ngọc Đức <duco060303@gmail.com>
2024-04-28 16:25:27 +0700

    update search debounce with popup friend

--- src/components/popupFriend/PopupFriend.jsx
+++ src/components/popupFriend/PopupFriend.jsx
@@ -1,10 +1,11 @@
 import InfiniteScroll from 'react-infinite-scroll-component';
 import './popupFriend.scss';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import Loading from "../loading/Loading";
 import useAxiosPrivate from '../../api/axiosPrivate';
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify"
+import { debounce } from 'lodash';
 
 function PopupFriend({ setIsShowPopupFriend, isPopupFriend = true }) {
     const axiosPrivate = useAxiosPrivate();
@@ -12,28 +13,50 @@ function PopupFriend({ setIsShowPopupFriend, isPopupFriend = true }) {
     const [hasNextPage, setHasNextPage] = useState(false);
     const [pageNum, setPageNum] = useState(1);
     const [isLoading, setIsLoading] = useState(true);
+    const [inputSearch, setInputSearch] = useState('');
+    const [inputDebounce, setInputDebounce] = useState('');
 
     useEffect(() => {
         const controller = new AbortController();
         const { signal } = controller;
 
-        const url = isPopupFriend ? '/friend-management/friend/' : '/friend-management/invite-friend/';
+        if (inputDebounce) {
+            const url = isPopupFriend ? '/friend-management/search-friend' : '/friend-management/search-invite';
 
-        axiosPrivate.get((`${url}${pageNum}`), { signal })
-            .then((response) => {
-                const data = response.data;
+            axiosPrivate.post((url), { 'username': inputDebounce, 'page': pageNum })
+                .then(response => {
+                    const data = response.data;
+                    setDataFriend((prev) => [...prev, ...data.data.datas]);
+                    setHasNextPage(pageNum <= data.data.maxPage - 1);
+                    setIsLoading(false);
+                })
+                .catch(err => {
+                    setIsLoading(false);
+                    if (signal.aborted) return;
 
-                setDataFriend((prev) => [...prev, ...data.data.datas]);
-                setHasNextPage(pageNum <= data.data.maxPage - 1);
-                setIsLoading(false);
-            })
-            .catch((error) => {
-                setIsLoading(false);
-                if (signal.aborted) return;
-            });
+                    toast.error("Lỗi không tìm được", {
+                        position: "top-right"
+                    });
+                });
 
+        } else {
+            const url = isPopupFriend ? '/friend-management/friend/' : '/friend-management/invite-friend/';
+
+            axiosPrivate.get((`${url}${pageNum}`), { signal })
+                .then((response) => {
+                    const data = response.data;
+
+                    setDataFriend((prev) => [...prev, ...data.data.datas]);
+                    setHasNextPage(pageNum <= data.data.maxPage - 1);
+                    setIsLoading(false);
+                })
+                .catch((error) => {
+                    setIsLoading(false);
+                    if (signal.aborted) return;
+                });
+        }
         return () => controller.abort();
-    }, [pageNum, axiosPrivate, isPopupFriend]);
+    }, [pageNum, axiosPrivate, isPopupFriend, inputDebounce]);
 
     const deleteListInvite = (friend_id) => {
         let newList = dataFriend.filter(item => item.friend_id !== friend_id);
@@ -76,6 +99,20 @@ function PopupFriend({ setIsShowPopupFriend, isPopupFriend = true }) {
     const handelClosePopup = () => {
         setIsShowPopupFriend(false)
     }
+    const debounceSearch = useCallback(debounce((nextValue) => fetchUser(nextValue), 300), [])// eslint-disable-line react-hooks/exhaustive-deps
+
+    const fetchUser = debounce((input) => {
+        setInputDebounce(input);
+        setDataFriend([]);
+        setPageNum(1);
+    }, 10)
+
+    const handelInput = (e) => {
+        setIsLoading(true);
+        const input = e.target.value
+        setInputSearch(input);
+        debounceSearch(input.trim());
+    }
 
     const content = dataFriend.map(user => {
         return (
@@ -102,7 +139,6 @@ function PopupFriend({ setIsShowPopupFriend, isPopupFriend = true }) {
 
     return (
         <>
-            {isLoading && <Loading />}
             <div className='popup-friend'>
                 <div className='content'>
                     <header>
@@ -111,13 +147,18 @@ function PopupFriend({ setIsShowPopupFriend, isPopupFriend = true }) {
                             Đóng
                         </button>
                     </header>
+                    <div className="search">
+                        <input type="text" placeholder="Nhập tên người bạn muốn tìm..." value={inputSearch} onChange={handelInput} />
+                    </div>
+                    {isLoading && <Loading />}
+                    {!isLoading && dataFriend.length <= 0 && <h3 style={{ textAlign: 'center' }}>{isPopupFriend ? 'Không có bạn bè' : 'Không có lời mời'}</h3>}
                     <InfiniteScroll
                         dataLength={dataFriend.length}
                         next={() => setPageNum(pageNum + 1)}
                         hasMore={hasNextPage}
                         loader={<Loading />}
                         className="main"
-                        height={window.innerHeight * 3 / 4}
+                        height={window.innerHeight * 3 / 4 - 100}
                     >
                         {content}
                     </InfiniteScroll>
