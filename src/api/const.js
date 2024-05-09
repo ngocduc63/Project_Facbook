ff1ed45974148686431c199d5965f678e5981aee
Nguyễn Ngọc Đức <duco060303@gmail.com>
2024-05-09 21:24:01 +0700

    update const value api

--- src/api/const.js
+++ src/api/const.js
@@ -1,7 +1,8 @@
 const api_dev = 'http://localhost:5000/';
 const api_product = 'https://facebook-api-5gjf.onrender.com/';
+const api = api_product;
 
-export const LINK_API = `${api_product}`;
-export const LINK_API_AVATAR = `${api_product}user-management/user/avatar/`;
-export const LINK_API_COVER = `${api_product}user-management/user/cover/`;
-export const LINK_API_POST = `${api_product}user-management/post/image/`;
+export const LINK_API = `${api}`;
+export const LINK_API_AVATAR = `${api}user-management/user/avatar/`;
+export const LINK_API_COVER = `${api}user-management/user/cover/`;
+export const LINK_API_POST = `${api}user-management/post/image/`;
